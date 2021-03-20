cb168b1588a940df8979617fc10427a98227dad7
nuppington-bit <harveyxthomison@gmail.com>
2021-03-20 12:25:39 -0700

    more stuff

--- entities/weapons/weapon_shotgun/cl_init.lua
+++ entities/weapons/weapon_shotgun/cl_init.lua
@@ -12,4 +12,4 @@
 
 include( "shared.lua" )
 SWEP.WepSelectIcon = surface.GetTextureID("vgui/splitbullet_glock")
-killicon.Add("weapon_glock", "HUD/killicons/splitbullet_pistol", Color( 255, 80, 0, 255 ) )
+killicon.Add("weapon_shotgun", "HUD/killicons/splitbullet_pistol", Color( 255, 80, 0, 255 ) )
